fd7f746f2fc2bb06a36e4d09f43ec79408ef4bdd
guidionisio5 <101677648+guidionisio5@users.noreply.github.com>
2022-09-08 19:13:53 -0300

    Initial commit

---
+++ index1.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+include './painel/conexao/conexao.php';
+
+
+
+$mail = $_POST['mail'];
+$password = $_POST['password']; //client
+
+$sql = "SELECT * FROM usuario WHERE mail = '$mail'";
+$search = mysqli_query($conexao,$sql);
+$array = mysqli_fetch_array($search);
+
+$passwordBase = $array['password']; // database
+$passwordEnc = $password; // encriptando password client
+
+if($passwordEnc == $passwordBase) {
+    $_SESSION['mailx'] = $mail;
+    header('Location: ./painel/index.php');
+} else {
+    header('Location: index.php?msg=2');
+}
+
+?>
