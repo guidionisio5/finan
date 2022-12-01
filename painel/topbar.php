0ddf0ca2c3a8646cdcdda9d00fc44be58490b3bd
guidionisio5 <101677648+guidionisio5@users.noreply.github.com>
2022-12-01 19:25:16 -0300

    arrumando codigo

--- painel/topbar.php
+++ painel/topbar.php
@@ -1,56 +1,60 @@
-<?php include '_select_nivel.php';?>
+<?php include '_select_nivel.php'; ?>
 <?php ?>
 <div class="headerbar">
 
-            <!-- LOGO -->
-            <div class="headerbar-left">
-                <a href="index.php" class="logo">
-                    <img alt="Logo" src="assets/images/logo.png" />
-                    <span>Dashboard</span>
+    <!-- LOGO -->
+    <div class="headerbar-left">
+        <a href="index.php" class="logo">
+            <img alt="Logo" src="assets/images/logo.png" />
+            <span>Dashboard</span>
+        </a>
+    </div>
+
+    <nav class="navbar-custom">
+
+        <ul class="list-inline float-right mb-0">
+
+
+            <li class="list-inline-item dropdown notif">
+                <a class="nav-link dropdown-toggle nav-user" data-toggle="dropdown" href="#" aria-haspopup="false" aria-expanded="false">
+                    <img src="assets/images/avatars/<?php if ($image != null) {
+                                                        echo $image . '.jpg';
+                                                    } else {
+                                                        echo 'avatar.png';
+                                                    } ?>" alt="Profile image" class="avatar-rounded">
                 </a>
-            </div>
+                <div class="dropdown-menu dropdown-menu-right profile-dropdown ">
+                    <!-- item-->
+                    <div class="dropdown-item noti-title">
+                        <h5 class="text-overflow">
+                            <small>Hello, admin</small>
+                        </h5>
+                    </div>
 
-            <nav class="navbar-custom">
+                    <!-- item-->
+                    <a href="profile.php" class="dropdown-item notify-item">
+                        <i class="fas fa-user"></i>
+                        <span>Profile</span>
+                    </a>
 
-                <ul class="list-inline float-right mb-0">
-                   
- 
-                    <li class="list-inline-item dropdown notif">
-                        <a class="nav-link dropdown-toggle nav-user" data-toggle="dropdown" href="#" aria-haspopup="false" aria-expanded="false">
-                            <img src="assets/images/avatars/<?php if($image != null){echo $image.'.jpg';}else{echo 'avatar.png';} ?>" alt="Profile image" class="avatar-rounded">
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right profile-dropdown ">
-                            <!-- item-->
-                            <div class="dropdown-item noti-title">
-                                <h5 class="text-overflow">
-                                    <small>Hello, admin</small>
-                                </h5>
-                            </div>
+                    <!-- item-->
+                    <a href="logout.php" class="dropdown-item notify-item">
+                        <i class="fas fa-power-off"></i>
+                        <span>Logout</span>
+                    </a>
+                </div>
+            </li>
 
-                            <!-- item-->
-                            <a href="profile.php" class="dropdown-item notify-item">
-                                <i class="fas fa-user"></i>
-                                <span>Profile</span>
-                            </a>
+        </ul>
 
-                            <!-- item-->
-                            <a href="logout.php" class="dropdown-item notify-item">
-                                <i class="fas fa-power-off"></i>
-                                <span>Logout</span>
-                            </a>
-                        </div>
-                    </li>
+        <ul class="list-inline menu-left mb-0">
+            <li class="float-left">
+                <button class="button-menu-mobile open-left">
+                    <i class="fas fa-bars"></i>
+                </button>
+            </li>
+        </ul>
 
-                </ul>
+    </nav>
 
-                <ul class="list-inline menu-left mb-0">
-                    <li class="float-left">
-                        <button class="button-menu-mobile open-left">
-                            <i class="fas fa-bars"></i>
-                        </button>
-                    </li>
-                </ul>
-
-            </nav>
-
-        </div>
+</div>
